f8aabc6e427928079994ee4edda6bed106ebf5fb
walker <walkerf082@gmail.com>
2023-12-07 19:22:36 -0300

    ainda tentando a parte memoria

--- testes/memory_test.cpp
+++ testes/memory_test.cpp
@@ -4,14 +4,48 @@
 #include <sys/shm.h>
 using namespace std;
  
- 
+
+int max = 1542;
+
+/**
+1º temos que simular um meio
+    max 1544 bytes
+    flag de acesso.
+
+2º tempo que implementar um pc
+
+3º temos que criar pacotes
+
+*/
+
 struct mem{
     int flag;
-    int max_size;
+    int max_size = 1542;
     int mem_id;
     key_t key;
 };
 
+
+struct pc{
+    long mac;
+};
+
+
+int write_to_memory(char* message, int mem_id){
+    // shmat to attach to shared memory
+    char *str = (char*)shmat(mem_id, (void*)0, 0);
+    snprintf(str, max, "%s", message )
+//    cout << "Data written in memory: " << str << endl;
+    shmdt(str);
+    return 0;
+}
+
+int read_from_memory(int mem_id){
+    char *str = (char*)shmat(mem_id, (void*)0, 0);
+    cout << "Data written in memory: " << str << endl;
+    shmdt(str);
+    return 0;
+}
 
  
 int main()
@@ -19,20 +53,12 @@ int main()
     struct mem *teste = (struct mem*)malloc(sizeof(struct mem)); 
 
     teste->key = ftok("shmfile", 65); 
- 
+
     // shmget returns an identifier in shmid
-    int shmid = shmget(teste->key, 1024, 0666 | IPC_CREAT);
- 
-    // shmat to attach to shared memory
-    char* str = (char*)shmat(shmid, (void*)0, 0);
- 
-    cout << "Write Data : ";
-    cin.getline(str, 1024);
- 
-    cout << "Data written in memory: " << str << endl;
- 
-    // detach from shared memory
-    shmdt(str);
- 
+    teste->mem_id = shmget(teste->key, teste->max_size, 0777 | IPC_CREAT);
+
+    
+    write_to_memory("teste123", teste->mem_id);
+    read_from_memory(teste->mem_id);
     return 0;
 }
